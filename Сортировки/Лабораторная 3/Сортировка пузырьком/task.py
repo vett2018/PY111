24587c82c2d4fc63f204655de37f4c2a7c4abcf3
maksfom <aa>
2023-03-01 19:43:34 +0300

    01.03

--- Сортировки/Лабораторная 3/Сортировка пузырьком/task.py
+++ Сортировки/Лабораторная 3/Сортировка пузырьком/task.py
@@ -1,7 +1,8 @@
 from typing import Sequence
+from operator import lt, gt #lt - больше, gt - меньше
+#from .task import sort
 
-
-def sort(container: Sequence[int]) -> Sequence[int]:
+def sort(container: Sequence[int], asc: bool = True) -> Sequence[int]:
     """
     Сортировка пузырьком
 
@@ -13,4 +14,23 @@ def sort(container: Sequence[int]) -> Sequence[int]:
     :param container: Массив, который надо отсортировать
     :return: Отсортированный в порядке возрастания массив
     """
-    ...  # TODO реализовать алгоритм сортировки пузырьком
+    # TODO реализовать алгоритм сортировки пузырьком
+    offset = 1
+    change = False
+    compare = gt if asc else lt
+    for len_mas in range(len(container)): #O(N)
+        for i in range(0, len(container)-offset): # была O(N), стала O(log(N)
+            #if container[i] > container[i+1]:
+            if compare(container[i], container[i+1]):
+                container[i], container[i+1] = container[i+1], container[i]
+                change = True
+            if not change: # if change is False
+                break
+        offset += 1
+    return container
+
+_set = [25, 6, 5, 99, 3, 2, 1, 0]
+sop = [99, 3, 2, 1, 0, 25, 6, 5]
+#print(sort(_set, True))
+print(sort(sop, False))
+

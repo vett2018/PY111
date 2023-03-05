2a7a6d031fb24e473397dcad88dc5e9a40cfe326
yy <yy>
2023-03-05 23:27:30 +0300

    05_03

--- O-нотация. Простые структуры данных/Практические задания/Стек/task.py
+++ O-нотация. Простые структуры данных/Практические задания/Стек/task.py
@@ -3,17 +3,22 @@ from typing import Any
 
 class Stack:
     def __init__(self):
-        self._stack = []
+        self._stack = list() #защищенный атрибут, что бы явно неподобраться к элементу
+        self._len = 0 #при иницилизации класса общая последовательность равна нулю
 
     def push(self, elem: Any) -> None:
+        # TODO реализовать операцию push
         """
         Добавление элемента в вершину стека
 
         :param elem: Элемент, который должен быть добавлен
         """
-        ...  # TODO реализовать операцию push
+        self._stack.append(elem)
+        self._len += 1
 
-    def pop(self) -> Any:
+        #любой элемент можем добавить в конец
+    def pop(self) -> Any: #нет ссылки сколько у нас элементов
+        # TODO реализовать операцию pop
         """
         Извлечение элемента из вершины стека.
 
@@ -21,9 +26,14 @@ class Stack:
 
         :return: Извлеченный с вершины стека элемент.
         """
-        ...  # TODO реализовать операцию pop
+        if not self._stack: #если стек не пустой
+            raise IndexError("Стек пустой")
+        self._stack.pop()
+        self._len -= 1
+       # не любой элемент мы можем извлеч с конца
 
-    def peek(self, ind: int = 0) -> Any:
+    def peek(self, ind: int = 0) -> Any: #передаем индекс элемента который у нас есть
+        # TODO реализовать операцию peek
         """
         Просмотр произвольного элемента, находящегося в стеке, без его извлечения.
 
@@ -34,12 +44,31 @@ class Stack:
 
         :return: Значение просмотренного элемента
         """
-        ...  # TODO реализовать операцию peek
+        if not isinstance(ind, int): #проверка на тип данных int
+            raise TypeError("Индекс не целый")
+
+        #проверка индекса лежит ли он в границах стека между нулем и длиной нашего элемента
+        if not 0 <= ind <= len(self._stack):
+        #if not 0 <= ind <= self._stack.__len__(): #аналогично работа с объектами
+        #if not 0 <= ind <= self._len
+            raise IndexError("Индекс вне границ стека")
+
+        invertirovaniy_index = -1 -ind
+        return self._stack(invertirovaniy_index)
+
 
     def clear(self) -> None:
+        # TODO реализовать операцию clear
         """ Очистка стека. """
-        ...  # TODO реализовать операцию clear
+        self._stack.clear() #удаление списка (очищаем все объекты которые внутри списка) сам список останется
+        self._len = 0
+        #self._stack = list() #переопределили сам автоматом удалил
+        # for value in self._stack: #второй способ
+        #     del value
 
     def __len__(self) -> int:
+        # TODO реализовать операцию __len__
         """ Количество элементов в стеке. """
-        ...  # TODO реализовать операцию __len__
+        print("Зашли в len")
+        # returnt len() #базовая хитрость
+        return self._len #ПЕРЕОПРЕДЕЛЕНИЕ СВОЕЙ СТРУКТУРЫ
